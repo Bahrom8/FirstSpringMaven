13815a1bdcaa3d67bb6c50a30ec56a12f8384123
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-05-02 14:21:29 -0700

    Maven

--- src/main/java/com/cydeo/firstspringmaven/FirstSpringMavenApplication.java
+++ src/main/java/com/cydeo/firstspringmaven/FirstSpringMavenApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstSpringMavenApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(FirstSpringMavenApplication.class, args);
     }
